632e868795797c6cfc17db0744379fad0f43dfc6
fawwazmayda <maydafawwaz@gmail.com>
2019-11-17 13:49:43 +0700

    3 Pie Charts

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
+fetch('http://10.0.74.239:8080/api2/piechart/2017').then(resp => resp.json())
+.then(d=> {
+    console.log(d)
+    drawPieChart(d,"myChart1")
+})
 fetch('http://10.0.74.239:8080/api2/piechart/2018').then(resp => resp.json())
 .then(d=> {
     console.log(d)
-    drawPieChart(d)
+    drawPieChart(d,"myChart2")
+})
+fetch('http://10.0.74.239:8080/api2/piechart/2019').then(resp => resp.json())
+.then(d=> {
+    console.log(d)
+    drawPieChart(d,"myChart3")
 })
 
-function drawPieChart(d){
+function drawPieChart(d,id){
     var label = []
     var data = []
     d.forEach(element => {
@@ -12,13 +22,14 @@ function drawPieChart(d){
         data.push(element['measure']*100)
     });
 
-    var ctx = document.getElementById("myChart").getContext('2d');
+    var ctx = document.getElementById(id).getContext('2d');
     var myChart = new Chart(ctx, {
     type: 'pie',
     data: {
     labels: label,
     datasets: [{
       backgroundColor: [
+          //Kasih Warna Bro
         "#2ecc71",
         "#3498db",
         "#95a5a6",
